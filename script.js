796268fdae80f2a5c975dbe01816cc31bd03be59
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-02-28 23:44:47 -0500

    Added event listener to clear button, where element was targetted using the active class that was added in the first button event listener.

--- script.js
+++ script.js
@@ -2,10 +2,34 @@ let wrapper = document.querySelector(".wrapper");
 let button = document.getElementById("button");
 let clearButton = document.getElementById("clear");
 
+/*This event listener will create a new div when the button with id of "button" is clicked. 
+It will also add an id to this new div*/
 button.addEventListener("click", function () {
   
   let newDiv = document.createElement("div");
   newDiv.id = "newDivId";
+  newDiv.classList = "active";
   wrapper.append(newDiv);
   
 });
+
+
+/*This event listener will create a node list of all the div elements with the class of "active" 
+and then produce an array of it using the Array.from() method. Then we use a forEach method to 
+cycle through the array-- that all the targeted elements will display as "none."*/
+clearButton.addEventListener("click", function () {
+  
+    let activeDivs = document.querySelectorAll('.active');
+    let divArray = Array.from(activeDivs);
+    
+    divArray.forEach(clearfunction);
+    
+    function clearfunction(element) {
+
+      element.style.display = "none";
+      
+    } 
+    
+    
+  })
+  
